f420807793ed120fb8fb8aff733a9a81a28f02be
Laila GH <laila.guzzon@gmail.com>
2021-05-07 10:28:14 -0300

    Update files

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Laila Guzzon Hussein Lailota on 07/05/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // o email de quem mandou a msg
+    let body: String
+}
